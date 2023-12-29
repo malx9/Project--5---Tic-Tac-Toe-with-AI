d2e862ea98e960508babab2143e9ce855c57b4c1
malx9 <144179748+malx9@users.noreply.github.com>
2023-12-29 03:09:02 +0000

    history deleted

--- .history/script_20231225204534.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-
-const humanScore = document.getElementById("player_score");
-const aiScore = document.getElementById("ai_score");
--- .history/script_20231225204912.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const humanScore = document.getElementById("player_score");
-const aiScore = document.getElementById("ai_score");
-
-const playerIndicator = document.getElementById("player_indicator");
-
-const square = document.querySelectorAll(".square");
